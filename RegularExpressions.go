ee6bfe4c00c25867af385caaa4a82b19a8b6394a
JEREMY HSIEN YEE YON - Student <G00330435@gmit.ie>
2017-11-14 15:29:27 +0000

    get random response (2)

--- RegularExpressions.go
+++ RegularExpressions.go
@@ -7,6 +7,7 @@ import (
     "fmt"
 	"math/rand"
 	"time"
+	// "regexp"
 
 )
 
@@ -16,17 +17,29 @@ func main(){
 
     rand.Seed(time.Now().UTC().UnixNano())
 
+
 	
 
     // prompt and get user input
-    fmt.Println("Enter String input:")
-    fmt.Scanf("%s\n", &input)
-
-    fmt.Println(elizaResponse(input))
+    // fmt.Println("Enter String input:")
+    // fmt.Scanf("%s\n", &input)
+	input = "People say I look like both my mother and father."
+    fmt.Printf("%s\n%s\n",input,elizaResponse(input))
+	input = "Father was a teacher."
+    fmt.Printf("%s\n%s\n",input,elizaResponse(input))
+	input = "I was my father’s favourite."
+    fmt.Printf("%s\n%s\n",input,elizaResponse(input))
+	input = "I’m looking forward to the weekend."
+    fmt.Printf("%s\n%s\n",input,elizaResponse(input))
+	input = "My grandfather was French!"
+    fmt.Printf("%s\n%s\n",input,elizaResponse(input))
  
 }
 
 func  elizaResponse(input string) string{
+
+	// matchedFather, err := regexp.MatchString("\\bfather\\b", input)
+	// fmt.Println(matchedFather, err)
 	
 	generalResponse := [] string{"I’m not sure what you’re trying to say. Could you explain it to me?",
 									"How does that make you feel?",
